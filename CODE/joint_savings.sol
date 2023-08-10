02a8e18dd6e56739d21deb15dc0cb4742055e8ff
isabelsimundic <isabelsimundic@gmail.com>
2023-08-10 11:47:01 +1000

    first commit

---
+++ CODE/joint_savings.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.17;
+
+contract JointSavings {
+    address payable public accountOne;
+    address payable public accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
+    function withdraw(uint amount, address payable recipient) public {
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+        require(contractBalance >= amount, "Insufficient funds!");
+
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+
+        recipient.transfer(amount);
+        lastWithdrawAmount = amount;
+        contractBalance = address(this).balance;
+    }
+
+    function deposit() public payable {
+        contractBalance = address(this).balance;
+    }
+
+    function setAccounts(address payable account1, address payable account2) public {
+        accountOne = account1;
+        accountTwo = account2;
+    }
+
+    function() external payable {}
+}
